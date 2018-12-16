d2e4ed590734e7e03f3a9627d3985874b08f8d06
jonahcrowlinson <jonah@rowlinson.eu>
2018-12-16 18:20:01 +0000

    added a few buttons. added a bit of js

---
+++ CastleBuildingGame/JS/index.js
@@ -0,0 +1,37 @@
+﻿var stonewalls = false;
+var woodenwalls = false;
+var squaretowers = false;
+var circulartowers = false;
+
+
+function woodenwalls {
+
+    if (woodenwalls = true) {
+        alert("You can only buy one set of walls!")
+    }
+    else {
+        if (stonewalls = true) {
+            removeStoneWalls()
+        }
+        else {
+            //WOODEN WALLS APPEAR
+        }
+    }
+
+}
+
+function stonewalls {
+
+    if (stonewalls = true) {
+        alert("You can only buy one set of walls!")
+    }
+    else {
+        if (woodenwalls = true) {
+            removeWoodenWalls()
+        }
+        else {
+            //WOODEN WALLS APPEAR
+        }
+    }
+
+}
